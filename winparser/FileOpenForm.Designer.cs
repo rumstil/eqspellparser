49da6cfa8c421237885207ca90c47a272823b046
rumstil <raidloot@gmail.com>
2015-08-23 09:04:01 -0700

    -file open window now sorted in reverse order with most recent file at the top

--- winparser/FileOpenForm.Designer.cs
+++ winparser/FileOpenForm.Designer.cs
@@ -88,7 +88,7 @@
             this.listView1.Location = new System.Drawing.Point(0, 35);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(660, 247);
-            this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Descending;
             this.listView1.TabIndex = 1;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
