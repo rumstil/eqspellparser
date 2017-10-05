145cab01d986f05d3f1543c4adfde514fc68a002
rumstil <raidloot@gmail.com>
2017-10-05 08:01:10 -0700

    -split enumerations off into separate files

--- core/AAParser.cs
+++ core/AAParser.cs
@@ -6,66 +6,9 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
+
 namespace EQSpellParser
 {
-    public enum AAExpansion
-    {
-        EQ = 0,
-        RoK,
-        SoV,
-        SoL,
-        PoP,
-        LoY,
-        LDoN,
-        GoD,
-        OoW,
-        DoN,
-        DoD,
-        PoR,
-        TSS,
-        TBS,
-        SoF,
-        SoD,
-        UF,
-        HoT,
-        VoA,
-        RoF,
-        CotF,
-        TDS,
-        TBM,
-        EoK,
-        RoS
-    }
-
-    public enum AAExpansionLong
-    {
-        Original = 0,
-        Ruins_of_Kunark,
-        Scars_of_Velious,
-        Shadows_of_Luclin,
-        Planes_of_Power,
-        Legacy_of_Ykesha,
-        Lost_Dungeons_of_Norrath,
-        Gates_of_Discord,
-        Omens_of_War,
-        Dragons_of_Norrath,
-        Depths_of_Darkhollow,
-        Prochecy_of_Ro,
-        The_Serpents_Spine,
-        The_Buried_Sea,
-        Secrets_of_Faydwer,
-        Seeds_of_Destruction,
-        Underfoot,
-        House_of_Thule,
-        Veil_of_Alaris,
-        Rain_of_Fear,
-        Call_of_the_Forsaken,
-        The_Darkened_Sea,
-        The_Broken_Mirror,
-        Empires_of_Kunark,
-        Ring_of_Scale
-    }
-
     public struct AASlot
     {
         public int SPA;
